8256890964ea7c2598c49253c69c1ac915e86d7e
Rubens Diego <rdsdo2011@gmail.com>
2021-02-15 16:07:00 -0300

    Teste de Join em Type orm

--- src/repository/permissoesRtasUusariosRepository.ts
+++ src/repository/permissoesRtasUusariosRepository.ts
@@ -1,5 +1,6 @@
 import {createQueryBuilder, getManager, In} from "typeorm";
 import {Usuarios} from "../entity/Usuarios";
+import {UsuariosRotas} from "../entity/UsuariosRotas";
 
 const buscarPermissoesRtasUusariosRepository = async () => {
 
@@ -8,10 +9,19 @@ const repostory = getManager()
 
 
 
-    buscarPrmissoes = await createQueryBuilder('Pessoas')
-        .leftJoinAndSelect('Pessoas.enderecosIdFK', 'persons')
-        .leftJoinAndSelect('Pessoas.emailsIdFK', 'items')
-        .leftJoinAndSelect('Pessoas.telefonesIdFK' , 'ok')
+
+    /*
+        buscarPrmissoes = await createQueryBuilder('Pessoas')
+            .leftJoinAndSelect('Pessoas.enderecosIdFK', 'persons')
+            .leftJoinAndSelect('Pessoas.emailsIdFK', 'items')
+            .leftJoinAndSelect('Pessoas.telefonesIdFK' , 'ok')
+            .getMany();
+    */
+
+    buscarPrmissoes = await createQueryBuilder('UsuariosRotas')
+        .leftJoin('UsuariosRotas.usuariosIdFK' , 'ur')
+        .leftJoinAndSelect('UsuariosRotas.rotasPermissoesIdFK' , 'ur2')
+
         .getMany();
 
 
